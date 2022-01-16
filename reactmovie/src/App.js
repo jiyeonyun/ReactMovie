23b7215090f07405c36d43253a1ac5eb6f7f29d5
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-17 00:36:54 +0900

    use Effect

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -4,14 +4,27 @@ import { useState,useEffect } from 'react';
 
 function App() {
   const [counter,setCounter] = useState(0);
+  const [keyWord,setKeyword] =useState('');
   const onClick = ()=> setCounter((prev)=>prev +1);
-  console.log('i run');
-  const iRunOnlyOnce = ()=>{
-    console.log('i run one');
-  }
-  useEffect(iRunOnlyOnce,[]);
+  const onChange = (event)=> setKeyword(event.target.value);
+  
+  useEffect(()=>{console.log('i run only once.');},
+  []);
+  useEffect(()=>{
+    if(keyWord !== "" && keyWord.length>5){
+      console.log('search for' , keyWord);
+    };
+  },[keyWord]);
+  useEffect(()=>{
+      console.log('i run when counter change');
+  },[counter]);
   return (
   <div>
+    <input 
+    value={keyWord}
+    onChange = {onChange} 
+    type='text' 
+    placeholder='Search here...' />
     <h1 className={styles.title}>{counter}</h1>
     <button onClick={onClick}>click me</button>
   </div>
