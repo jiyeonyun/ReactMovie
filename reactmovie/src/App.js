c91d7f963ff4ddc19e9bdad006a8bedb958e6e46
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-16 23:44:37 +0900

    uesEffect

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,10 +1,15 @@
 import Button from './Button';
 import styles from './App.module.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 function App() {
   const [counter,setCounter] = useState(0);
   const onClick = ()=> setCounter((prev)=>prev +1);
+  console.log('i run');
+  const iRunOnlyOnce = ()=>{
+    console.log('i run one');
+  }
+  useEffect(iRunOnlyOnce,[]);
   return (
   <div>
     <h1 className={styles.title}>{counter}</h1>
