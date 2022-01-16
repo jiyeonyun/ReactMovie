f10380842ee3ca76f4ba9841132ffe9e37a2b96c
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-16 18:49:42 +0900

    effect

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,10 +1,14 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState } from 'react';
+
 function App() {
+  const [counter,setCounter] = useState(0);
+  const onClick = ()=> setCounter((prev)=>prev +1);
   return (
   <div>
-    <h1 className={styles.title}>Welcome back!</h1>
-    <Button text={"continue"} />
+    <h1 className={styles.title}>{counter}</h1>
+    <button onClick={onClick}>click me</button>
   </div>
   );
 }
