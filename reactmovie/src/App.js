9ce05b80235613a50a606ec8b73982d2fab79044
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-18 16:01:26 +0900

    set Todos

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,12 +1,28 @@
 import { useState,useEffect } from 'react';
 
 function App() {
-
+  const[todo,setTodo] = useState('');
+  const[todos ,setTodos] = useState([]);
+  const onChange = (event)=>{ setTodo(event.target.value);};
+  const onSubmit = (event)=>{
+    event.preventDefault();
+    if(todo === ''){
+      return;
+    }
+    setTodos((currentArray) =>[todo,...currentArray]);
+    setTodo('');
+  };
   return (
   <div>
+    <h1>My To Dos ({todos.length})</h1>
+    <form onSubmit={onSubmit}>
     <input 
+    onChange={onChange}
     type='text' 
+    value={todo}
     placeholder='Write your to do...' />
+    <button>Add To Do</button>
+    </form>
   </div>
   );
 }
