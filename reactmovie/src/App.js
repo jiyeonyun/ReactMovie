53d959e1cc4a7fe437fcd0958414342515db5e8d
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-22 22:52:52 +0900

    fetch json

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -1,53 +1,21 @@
 import { useState,useEffect } from 'react';
 
 function App() {
-  const[loading,setLoading] = useState(true);
-  const[onof,setOnof] = useState(false);
-  const [coins,setCoins] =useState([]);
-  const [selectOp,setSelectOp] = useState('');
-  const [money,setMoney] = useState('');
+  const [loading , setLoading] = useState(true);
+  const [movies ,setMovies] = useState([]);
   useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json)
-      setLoading(false);
-    });
-  },[]);
-  const onchange = (event)=>{
-    setSelectOp(event.target.value);
-  };
-  const onSubmit = (event)=>{
-      event.preventDefault();
-  }
-  const onMoney = (event)=>{
-    setMoney(event.target.value);
-  }
-  const onofChange = ()=>{
-    setOnof(true);
-    }
-  return (
-  <div>
-  <h1>The Coins!({coins.length})</h1>
-  {loading ? <strong>Loading...</strong> 
-  :<div>
-    <form onSubmit={onSubmit}>
-    <input
-    onChange={onMoney}
-    type='number'
-    />
-    <span>USD</span>
-    </form>
-    <select onChange={onchange}>
-    <option>Select coin</option>
-  {coins.map((coin)=><option value={[coin.quotes.USD.price]}>{coin.name}({coin.symbol}) : {coin.quotes.USD.price}USD</option>)}
-  </select>
-  <button onClick={onofChange}>calculate</button>
-  {onof ? <h2>you can buy {Math.floor(money /selectOp)} coins</h2> : null}
-  </div>
-  }
-  </div>
-  );
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    ).then((response) => response.json())
+    .then((json) => setMovies(json.data.movies))
+    setLoading(false);
+  },[])
+  console.log(movies);
+  return(
+    <div>
+      {loading ? <h1>Loading...</h1> : null}
+    </div>
+  )
+  
 }
 
 export default App;
