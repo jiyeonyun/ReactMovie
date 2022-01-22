f633630b64e71e8c14b50928039715ccb97dcaee
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-22 22:56:20 +0900

    async

--- reactmovie/src/App.js
+++ reactmovie/src/App.js
@@ -3,11 +3,17 @@ import { useState,useEffect } from 'react';
 function App() {
   const [loading , setLoading] = useState(true);
   const [movies ,setMovies] = useState([]);
-  useEffect(()=>{
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    ).then((response) => response.json())
-    .then((json) => setMovies(json.data.movies))
+  const getMovise = async() =>{
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
     setLoading(false);
+  }
+  useEffect(()=>{
+    getMovise();
   },[])
   console.log(movies);
   return(
