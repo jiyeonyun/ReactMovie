7accfb8535d60621a5b86552436d67751df58483
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-25 19:42:54 +0900

    Router2

--- reactmovie/src/routes/Home.js
+++ reactmovie/src/routes/Home.js
@@ -3,8 +3,8 @@ import Movies from '../components/Movies';
 
 function Home(){
     const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
     const json = await (
       await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
