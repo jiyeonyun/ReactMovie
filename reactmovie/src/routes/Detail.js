82a43577d76d71d4f0d5db3500541126dd1dbf17
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-03 17:49:49 +0900

    detail css

--- reactmovie/src/routes/Detail.js
+++ reactmovie/src/routes/Detail.js
@@ -1,6 +1,8 @@
 import {useParams} from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import "../style.css";
+import "../detail.css";
+import Header from '../components/header';
 
 function Detail(){
     const {id} = useParams();
@@ -13,6 +15,7 @@ function Detail(){
             setMovies(json.data.movie);
             setLoading(false);
     };
+    console.log(movies);
     useEffect(()=>{
         getMovie();
     },[]);
@@ -25,19 +28,25 @@ function Detail(){
                 <img src='https://i.pinimg.com/originals/7e/b2/3b/7eb23b8fa4b6c374504ce2fb9d9c5399.gif'/>
                 </div>
         ) : (
-            <div>
-            <img src={movies.large_cover_image}></img>
-            <h1>{movies.title_long}</h1>
-            <h2>{movies.year}</h2>
-            <h3>{movies.rating}</h3>
-            <p>{movies.runtime} minitues</p>
-            <ul>
-                {movies.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            <p>{movies.description_full}</p>
-
+            <div className='detail' style={{backgroundImage: `url(${movies.background_image})`, backgroundRepeat : 'no-repeat',backgroundSize:'cover'}}>
+              <Header/>
+              <div className='wrap'>
+              <div className='card'>
+              <img src={movies.large_cover_image}></img>
+              <div className='descCard'>
+                <h1>{movies.title_long}</h1>
+                <h2>{movies.year}</h2>
+                <h3>⭐️ {movies.rating}</h3>
+                <h4>⏰ {movies.runtime} minitues</h4>
+                <ul>
+                    {movies.genres.map((g) => (
+                      <li key={g}>#{g}</li>
+                    ))}
+                  </ul>
+                <p>{movies.description_full}</p>
+              </div>
+              </div>
+              </div>
             </div>
         )}
       </div>
