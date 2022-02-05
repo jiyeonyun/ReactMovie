07221848384aa57c192f272b77676a0df1fbd0f0
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-05 21:43:30 +0900

    genre

--- reactmovie/src/routes/Genre.js
+++ reactmovie/src/routes/Genre.js
@@ -1,27 +1,32 @@
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom"
+import Gmovies from '../components/Gmovies';
+import Header from '../components/header';
+import "../style.css";
+import "../detail.css";
 
 function Genre(){
-  const {group} = useParams();
+  const {group,page} = useParams();
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const List_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   const getMovies = async () => {
     // console.log(`getMovie`)
     const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?${group}&sort_by=rating`)
+      await fetch(`https://yts.mx/api/v2/list_movies.json?page=${page}&${group}&sort_by=rating`)
     ).json();
     setMovies(json.data.movies);
     setLoading(false);
-    // console.log(movies[0])
   }
+  console.log(movies)
 
   useEffect(() => {
     // console.log(`useEffect`)
     getMovies();
     return ;
-  }, [group])
+  }, [group,page])
 
   return (
     <div>
@@ -32,8 +37,38 @@ function Genre(){
           </div>
   ) : (
       <div>
-        <h1>hi</h1>
+            <Header />
+          <div className='genreWrap'>
+            {movies.map((movie) =>(
+          <Gmovies 
+          key={movie.id}
+          id={movie.id}
+          CoverImage={movie.medium_cover_image} 
+          title={movie.title} 
+          summary={movie.summary}
+          year={movie.year} 
+          genres={movie.genres}
+          rating={movie.rating}
+          runtime={movie.runtime}/>
+          ))}
+          </div>
+          <div className='footer'>
+            <div className='list'>
+              {
+                List_arr.map((lst) => {
+                  return (
+                    <Link
+                      key={lst}
+                      to={`/page/${group}/${lst}`}
+                    >
+                      {lst}</Link>
+                  )
+                })
+              }
+            </div>
+          </div>
       </div>
+      
   )}
   </div>
   )
