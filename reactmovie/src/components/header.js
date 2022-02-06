928eaa3013598ee1050a3a0c59261b88b7bd680f
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-06 20:18:46 +0900

    header scroll

--- reactmovie/src/components/header.js
+++ reactmovie/src/components/header.js
@@ -1,14 +1,31 @@
 import {useParams} from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch,faChevronLeft,faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import "../style.css";
 import { Link } from "react-router-dom";
 import { Group_obj, Group_key_arr } from "../atom/navbar";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 function Header(props){
+    const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장 
+    const [ScrollActive, setScrollActive] = useState(true); 
+    function handleScroll() { 
+        if(ScrollY > 20) {
+            setScrollY(window.pageYOffset);
+            setScrollActive(false);
+        } else {
+            setScrollY(window.pageYOffset);
+            setScrollActive(true);
+        }
+    }
+    useEffect(() => {
+        function scrollListener() {  window.addEventListener("scroll", handleScroll); } //  window 에서 스크롤을 감시 시작
+        scrollListener(); // window 에서 스크롤을 감시
+        return () => { window.removeEventListener("scroll", handleScroll); }; //  window 에서 스크롤을 감시를 종료
+    });
     return(
-        <nav className='header'>
+        <nav className={ScrollActive ? "header" : "headerChange"}>
             {/*  Page Name */}
             <h1 className='header_h1'>
                 <Link to={"/"} >YUNFLEX</Link>
@@ -26,12 +43,6 @@ function Header(props){
                         )
                     })
                 }
-            </div>
-            <div className='searchBar'>
-              <form>
-                <input placeholder='Search Movie!'></input>
-                <button><FontAwesomeIcon icon={faSearch} className="search"/></button>
-              </form>
             </div>
           </nav>
     )
