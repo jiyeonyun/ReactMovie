b037185bd317ea35e7fcd6ff6c5cd5efa35deb75
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-23 18:21:53 +0900

    delete all

--- reactMovieRenewal/src/App.js
+++ reactMovieRenewal/src/App.js
@@ -1,23 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './routes/Home';
-import Detail from './routes/Detail';
-import Genre from './routes/Genre';
 
 function App() {
   return (
-  <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route exact path="/">
-          <Home />
-      </Route>
-      <Route path='/movie/:id'>
-        <Detail />
-      </Route>
-      <Route path ='/page/:group/:page'>
-        <Genre />
-      </Route>
-    </Switch>
-  </Router>
+  <h1>hi</h1>
   );
 }
 
