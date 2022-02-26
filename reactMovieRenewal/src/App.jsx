14e7b12adebe57c00ed6ed76a4bd4be8484b99e0
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-26 17:06:08 +0900

    search

--- reactMovieRenewal/src/App.jsx
+++ reactMovieRenewal/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './routes/home/home';
 import Detail from './routes/detail/detail';
 import Genre from './routes/genre/genre';
+import Header from './components/header/header';
+import Search from './routes/search/search';
 
 function App() {
   const[movies, setMovies] = useState([]);
@@ -14,22 +16,32 @@ function App() {
       )
     ).json();
     setMovies(json.data.movies);
-}
-useEffect(()=>{
-  getMovies()
-  window.scrollTo(0, 0);
-},[page])
+      };
+  const movieSearch = async (input) => {
+        const json = await (
+                await fetch(`https://yts.mx/api/v2/list_movies.json?query_term="${input}"&sort_by=year`)  
+            ).json();
+            setMovies(json.data.movies);
+        }
+        useEffect(()=>{
+            getMovies()
+            window.scrollTo(0, 0);
+        },[page])
 return (
   <Router basename={process.env.PUBLIC_URL}>
+    <Header movieSearch={movieSearch}/>
     <Switch>
       <Route exact path="/">
-        <Home movies={movies} page={page} setPage={setPage}/>
+        <Home movies={movies} page={page} setPage={setPage} />
       </Route>
       <Route path='/movie/:id'>
         <Detail />
       </Route>
       <Route path='/genre/:genres'>
         <Genre />
+      </Route>
+      <Route path='/search'>
+        <Search movies={movies} page={page} setPage={setPage}/>
       </Route>
     </Switch>
   </Router>
